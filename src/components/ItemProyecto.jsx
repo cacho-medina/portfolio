18ce147e3525d5c44b2fcfc3c6c5cab1c59d8ef1
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-02-22 08:55:43 -0300

    se agrego desc a itemProyecto, se rediseño el componente itemProyecto y se hicieron cambios en css

--- src/components/ItemProyecto.jsx
+++ src/components/ItemProyecto.jsx
@@ -1,38 +1,37 @@
 import { FaGithub } from "react-icons/fa";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import Card from "react-bootstrap/Card";
 
 function ItemProyecto({ proyecto }) {
     const { name, desc, repo, url, img } = proyecto;
     return (
-        <div className="col-12 col-md-5 m-1 bg-light-blue py-2 d-flex flex-column justify-content-between align-items-center proyectoContainer">
-            <div className="proyecto">
-                <img
-                    src={img}
-                    alt={`imagen proyecto ${name}}`}
-                    className="img-fluid object-fit-cover"
-                />
-            </div>
-            <h2 className="display-5 text-center pointer-normal">{name}</h2>
-            <div className="mx-auto d-flex gap-3">
-                <a
-                    href={repo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="icono"
-                >
-                    <FaGithub />
-                </a>
-                <a
-                    href={url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="icono"
-                >
-                    <FaExternalLinkAlt />
-                </a>
-            </div>
-            <p>{desc}</p>
-        </div>
+        <Card className="col-12 col-sm-8 col-md-5 m-1 bg-light-blue px-0 text-grey proyectoContainer">
+            <Card.Img variant="top" src={img} className="card-img" />
+            <Card.Body className="d-flex flex-column justify-content-between align-items-center">
+                <Card.Title className="display-6 text-center pointer-normal">
+                    {name}
+                </Card.Title>
+                <Card.Text className="desc pointer-normal">{desc}</Card.Text>
+                <div className="mx-auto d-flex gap-3">
+                    <a
+                        href={repo}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="icono"
+                    >
+                        <FaGithub />
+                    </a>
+                    <a
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="icono"
+                    >
+                        <FaExternalLinkAlt />
+                    </a>
+                </div>
+            </Card.Body>
+        </Card>
     );
 }
 
